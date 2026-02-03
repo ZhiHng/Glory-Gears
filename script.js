dbfd92caea70ea3826f72cbb71d0b3f88fc2510a
ZhiHng <s10271139@connect.np.edu.sg>
2026-02-03 22:32:34 +0800

    added some more images, completed index for weapons and edited story code a little. Started on materials index.

--- script.js
+++ script.js
@@ -106,6 +106,7 @@ document.addEventListener("keydown", function(event) {
 });
 
 //---------------GAME LOGIC----------------
+//STORY
 const story = {
     chapter1_1: {
         title: "Chapter 1 - A Strange Sword",
@@ -114,7 +115,7 @@ const story = {
         "It's been a long time since you have experienced freedom, ever since being caught and put into jail as an accomplice for a crime, cleaning up the crime scene. You are determined to start anew.",
         "You: First things first, I must find a way to sustain myself. I need money."
         ],
-        image: "pictures/jail.jpeg",
+        image: "images/backgrounds/jail.jpeg",
         choices: [
         { text: "Ask around houses for a job", next: "chapter1_1_1" },
         { text: "Head to the adventurer's guild", next: "chapter1_1_2" }
@@ -126,7 +127,7 @@ const story = {
         text: [
         "Everyone rejects you, leaving you with no choice but to go to the adventurer's guild. A kind soul gives you some gold to help support your journey."
         ],
-        image: "pictures/town.jpeg",
+        image: "images/backgrounds/town.png",
         choices: [
         { text: "Continue", next: "chapter1_2" }
         ]
@@ -137,7 +138,7 @@ const story = {
         text: [
         "You sign up as an adventurer. Having just started out, you cannot accept high-rank quests."
         ],
-        image: "pictures/guild.jpeg",
+        image: "images/backgrounds/town.png",
         choices: [
         { text: "Continue", next: "chapter1_2" }
         ]
@@ -160,7 +161,7 @@ const story = {
         "After the bizarre incident, you stand there, shocked and bewildered, wondering what is even happening. Your once-rejected sword doesn't seem the same anymore. It looks as if it has been upgraded.",
         "You: How did it… do that? Is this what that passerby meant? That was insane."
         ],
-        image: "pictures/forest.jpeg",
+        image: "images/backgrounds/forest.png",
         choices: [
         { text: "Head back to the adventurer's guild", next: "chapter1_2_1" },
         { text: "Swiftly exit the area and ask locals about the sword's value", next: "chapter1_2_2" }
@@ -172,7 +173,7 @@ const story = {
         text: [
         "You head back to the adventurer's guild and claim your reward for the quest you completed. While wandering around town, some traders catch sight of your sword. They crowd around you and start offering large sums of money for it."
         ],
-        image: "pictures/town.jpeg",
+        image: "images/backgrounds/town.png",
         choices: [
         { text: "Continue", next: "chapter1_3" }
         ]
@@ -185,7 +186,7 @@ const story = {
         "You return to the trader who initially had zero interest in the sword. His facial expression changes drastically from dispassionate to excited. He hurriedly offers you a lump sum of gold, telling you it's a good deal for both of you. You stand there, clueless and stunned.",
         "You: Wasn't this sword worth nothing?"
         ],
-        image: "pictures/market.jpeg",
+        image: "images/backgrounds/town.png",
         choices: [
         { text: "Continue", next: "chapter1_3" }
         ]
@@ -207,7 +208,7 @@ const story = {
         "You: Wow… that's a lot to take in. Is he still out there?",
         "Passerby: Do you want to hear the story of the legendary heroes' battle?"
         ],
-        image: "pictures/shop.jpeg",
+        image: "images/backgrounds/shop.png",
         choices: [
         { text: "Sure, tell me about it.", next: "chapter1_3_1" },
         { text: "It's fine. Just tell me what to do next.", next: "chapter1_3_2" }
@@ -228,7 +229,7 @@ const story = {
         "Little did they know, some light was still reachable inside the cave. Though not enough energy to climb out, the final clone lay dormant there for eternity. All the heroes could hope for was that no one would ever find the legendary weapons and expose them to sunlight, powering the weapons and bringing the clones back.",
         "In the event that it does happen, may Peruare's power have withered enough over the years for a brave new soul to finish what they set out to do that fateful day."
         ],
-        image: "pictures/shop.jpeg",
+        image: "images/backgrounds/grasslands.png",
         choices: [
         { text: "Continue", next: "chapter1_3_2" }
         ]
@@ -242,7 +243,7 @@ const story = {
         "You: Alright, I got you.",
         "You prepare to set out the next day."
         ],
-        image: "pictures/inn.jpeg",
+        image: "images/backgrounds/shop.png",
         choices: [
         { text: "Continue", next: "end" }
         ]
@@ -253,7 +254,7 @@ const story = {
         text: [
         "The passerby gives you some money to buy heat-resistant equipment and requests that you head to the desert to retrieve the Fire Scythe. You travel long distances until you reach a forgotten temple filled with monsters."
         ],
-        image: "pictures/desert_temple.jpeg",
+        image: "images/backgrounds/desert.png",
         choices: [
         { text: "Kill the monsters", next: "chapter2_1_1" },
         { text: "Sneak past them", next: "chapter2_1_2" }
@@ -272,9 +273,9 @@ const story = {
         "Thinking it's over, you are stunned when, as the dirt is removed, a sharp blade grows from the spear, slicing the clone's head in half.",
         "Taking this opportunity, you grab the newly transformed scythe and the crystal sword and flee the scene. Looking back, you spot two clones this time, not chasing."
         ],
-        image: "pictures/fire_temple.jpeg",
+        image: "images/backgrounds/desert.png",
         choices: [
-        { text: "Continue", next: "chapter3_1" }
+        { text: "Continue", next: "end" }
         ]
     },
 
@@ -284,13 +285,21 @@ const story = {
         "You sneak past the monsters, carefully searching for the Fire Scythe. After wandering for a while, you notice the number of monsters dwindling. A sense of unease settles in as your sword steadily glows brighter.",
         "Eventually, you find a massive room with a spear embedded in a pedestal at its center. You stop in front of it and attempt to yank it out, but all your efforts are in vain.",
         "The clone appears from the shadows, saying nothing, staring you down. You can clearly feel it's aura at an exponentially larger scale since the last meeting. It spits out a monster head from its mouth and draws its sword, inviting you to a battle you cannot decline.",
-        "(Fight occurs, depending on probability)",
-        "Lose - Restart the story.",
-        "Win - You overpower the clone and beat him to a pulp. In desperation, he dashes toward the spear and yanks it from the pedestal in a split second.",
+        ],
+        image: "images/backgrounds/desert.png",
+        choices: [
+        { text: "Fight", next: "chapter2_2" }
+        ]
+    },
+
+    chapter2_2: {
+        title: "",
+        text: [
+        "You overpower the clone and beat him to a pulp. In desperation, he dashes toward the spear and yanks it from the pedestal in a split second.",
         "Thinking it's over, you watch as a sharp blade grows from the spear, slicing the clone's head in half. Taking this opportunity, you grab the newly transformed scythe and flee the scene.",
         "Looking back, you spot two clones this time, not chasing."
         ],
-        image: "pictures/fire_temple.jpeg",
+        image: "images/backgrounds/desert.png",
         choices: [
         { text: "Continue", next: "end" }
         ]
@@ -301,7 +310,7 @@ const story = {
         text: [
         "You reach the temple, and it transports you into the sky through a rising platform. You see the bow right in the middle of the room."
         ],
-        image: "pictures/sky_temple.jpeg",
+        image: "images/backgrounds/grasslands.png",
         choices: [
         { text: "Take the long, safer-looking way.", next: "chapter3_1_1" },
         { text: "Take the faster, booby-trapped-looking path.", next: "chapter3_1_2" }
@@ -315,7 +324,7 @@ const story = {
         "You: Aren't only two legendary weapons activated so far?",
         "When the clones approach the bow, they trigger a trap, spawning monsters around it. Taking this opportunity while the clones are busy, you sweep in, take them out swiftly, grab the bow, and flee."
         ],
-        image: "pictures/sky_path_safe.jpeg",
+        image: "images/backgrounds/grasslands.png",
         choices: [
         { text: "Continue", next: "chapter3_2" }
         ]
@@ -324,13 +333,22 @@ const story = {
     chapter3_1_2: {
         title: "",
         text: [
-        "There is a 50% chance to trigger a trap that spawns monsters (x3).",
-        "Lose once - Restart the story",
-        "Win three times - You finally see the bow again and begin to approach it. As you pick up the bow, you notice something moving in the shadows. Upon closer inspection, you realize it is one of the clones.",
+        "You trigger a trap and monsters spawn"
+        ],
+        image: "images/backgrounds/grasslands.png",
+        choices: [
+        { text: "Fight", next: "chapter3_1_3" }
+        ]
+    },
+
+    chapter3_1_3: {
+        title: "",
+        text: [
+        "You finally see the bow again and begin to approach it. As you pick up the bow, you notice something moving in the shadows. Upon closer inspection, you realize it is one of the clones.",
         "You make a run for it, and both clones chase after you. Looking forward, you see a third clone flanking you and manage to parry it just in time.",
         "You: Aren't only two legendary weapons activated so far?"
         ],
-        image: "pictures/sky_path_trap.jpeg",
+        image: "images/backgrounds/grasslands.png",
         choices: [
         { text: "Continue", next: "chapter3_2" }
         ]
@@ -347,11 +365,150 @@ const story = {
         "Passerby: That means they've found the energy spectre, one of the legendary weapons. As of now, only one weapon remains before all the legendary weapons are reclaimed. The last weapon is the water staff, deep underground in the sea. I think it would be best if I accompanied you on this trip. After all, the extra clone makes things harder, and I think you could use some help.",
         "You take a well-needed rest and plan to set out the next day"
         ],
-        image: "pictures/town_night.jpeg",
+        image: "images/backgrounds/town.png",
         choices: [
         { text: "Continue", next: "end" }
         ]
     }
 };
 
+//INVENTORY
+const weapons = {
+    crystalPiercer: {
+        name: "Crystal Piercer",
+        description: "A legendary mystical sword once weilded by the hero of light. It is known for its lightweight build and razor sharp blade. Can be obtained from Chapter 1.",
+        element: "Light",
+        range: "Short",
+        damage: 50,
+        passive: "Damage against demonic enemies +50%",
+        image: "images/weapons/crystalPiercer.png",
+        price: 0
+    },
+
+    infernalCleaver: {
+        name: "Infernal Cleaver",
+        description: "A legendary mystical scythe once weilded by the hero of flame. It is known for emmiting scorching heat and its dual forms. Can be obtained from Chapter 2.",
+        element: "Fire",
+        range: "Long",
+        damage: 40,
+        passive: "Damage against non-boss enemies +50%",
+        image: "images/weapons/infernalCleaver.png",
+        price: 0
+    },
+
+    tempestArcs: {
+        name: "Tempest Arcs",
+        description: "A legendary mystical bow once weilded by the hero of electric. It is known for its accuracy and its ability to transform into a phoenix. Can be obtained from Chapter 3.",
+        element: "Fire",
+        range: "Very Long",
+        damage: 35,
+        passive: "Winning Chance +20%",
+        image: "images/weapons/tempestArcs.png",
+        price: 0
+    },
+
+    sturdyBroadsword: {
+        name: "Sturdy Broadsword",
+        description: "An ordinary broadsword that every novice adventurer can use. There's nothing special about it but it gets the job done. Can be obtained from the shop or by exploring.",
+        element: "None",
+        range: "Short",
+        damage: 10,
+        passive: "None",
+        price: 20
+    },
+
+    sturdySpear: {
+        name: "Sturdy Spear",
+        description: "An ordinary spear that every novice adventurer can use. There's nothing special about it but it gets the job done. Can be obtained from the shop or by exploring.",
+        element: "None",
+        range: "Long",
+        damage: 10,
+        passive: "None",
+        price: 25
+    },
+
+    sturdyBow: {
+        name: "Sturdy Bow",
+        description: "An ordinary bow that every novice adventurer can use. There's nothing special about it but it gets the job done. Can be obtained from the shop or by exploring.",
+        element: "None",
+        range: "Very Long",
+        damage: 8,
+        passive: "None",
+        price: 15
+    },
+
+    frostSpear: {
+        name: "Frost Spear",
+        description: "A long spear that is imbued with ice elemental powers, forged from a Frost Gem. It is capable of emmiting ice elemental attacks to a reasonable extent. Can be obtained from exploring.",
+        element: "Ice",
+        range: "Long",
+        passive: "Damage against small enemies +10%",
+        price: 80
+    },
+
+    frostSword: {
+        name: "Frost Sword",
+        description: "A sleek sword that is imbued with ice elemental powers, forged from a Frost Gem. It is capable of emmiting ice elemental attacks to a reasonable extent. Can be obtained from shop.",
+        element: "Ice",
+        range: "Short",
+        passive: "Elemental damage x1.5",
+        price: 85
+    },
+
+    flameSpear: {
+        name: "Flame Spear",
+        description: "A long spear that is imbued with fire elemental powers, forged from a Flame Gem. It is capable of emmiting fire elemental attacks to a reasonable extent. Can be obtained from exploring.",
+        element: "Fire",
+        range: "Long",
+        passive: "Damage against large enemies + 10%",
+        price: 85
+    },
+
+    flameSword: {
+        name: "Flame Sword",
+        description: "A sleek sword that is imbued with fire elemental powers, forged from a Flame Gem. It is capable of emmiting fire elemental attacks to a reasonable extent. Can be obtained from shop.",
+        element: "Fire",
+        range: "Short",
+        passive: "Cleaniness damage bonus +10%",
+        price: 80
+    },
+};
+
+const materials = {
+    sponge: {
+
+    },
+
+    crystal: {
+        
+    },
+
+    flameGem: {
+        
+    },
+
+    frostGem: {
+        
+    },
+
+    shockGem: {
+        
+    },
+
+    energyGem: {
+        
+    },
+
+    ring: {
+        
+    },
+
+    bracelet: {
+        
+    },
+
+    magicOrb: {
+        
+    },
+};
 
